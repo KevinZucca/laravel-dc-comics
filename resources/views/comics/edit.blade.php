7d863400765b2d4c148ec65c76a342c0bc7dd582
KevinZucca <kev.94@live.it>
2023-05-17 14:17:19 +0200

    feat: modal added

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -56,13 +56,32 @@
 
       <button class="btn btn-primary" type="submit">save</button>
 
-      <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button class="btn btn-danger" type="submit">delete</button>
+    
       </form>
 
 
+      <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          delete
+        </button>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">This will delete the file permanently. Do you want to continue?</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+        
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="submit" class="btn btn-danger">Yes</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </form>
 
 </div>
