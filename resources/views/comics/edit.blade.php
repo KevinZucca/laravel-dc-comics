571bffc1e81b52dd94b7433826c2635e4e4480f6
KevinZucca <kev.94@live.it>
2023-05-19 14:03:01 +0200

    feat: added new comics

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -9,7 +9,7 @@
         @method('PUT')
         <div class="form-group">
           <label for="exampleFormControlInput1">Title</label>
-          <input type="text" class="form-control @error('title') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit title" name="title" value="{{old('title')}}">
+          <input type="text" class="form-control @error('title') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit title" name="title" value="{{old('title') ? : $comic->title}}" required>
           @error('title')
           <div class="invalid-feedback">
             {{$message}}
@@ -19,7 +19,7 @@
 
         <div class="form-group">
             <label for="exampleFormControlInput1">Thumb</label>
-            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit thumb link" name="thumb" value="{{old('thumb')}}">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit thumb link" name="thumb" value="{{old('thumb') ? : $comic->thumb}}">
             @error('thumb')
             <div class="invalid-feedback">
               {{$message}}
@@ -29,7 +29,7 @@
 
           <div class="form-group">
             <label for="exampleFormControlInput1">Price</label>
-            <input type="text" class="form-control @error('price') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit price" name="price" value="{{old('price')}}">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit price" name="price" value="{{old('price') ? : $comic->price}}" required>
             @error('price')
             <div class="invalid-feedback">
               {{$message}}
@@ -39,7 +39,7 @@
 
           <div class="form-group">
             <label for="exampleFormControlInput1">Series</label>
-            <input type="text" class="form-control @error('series') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit series name" name="series" value="{{old('series')}}">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit series name" name="series" value="{{old('series') ? : $comic->series}}">
             @error('series')
             <div class="invalid-feedback">
               {{$message}}
@@ -49,7 +49,7 @@
 
           <div class="form-group">
             <label for="exampleFormControlInput1">Sale date</label>
-            <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit sale date" name="sale_date" value="{{old('sale_date')}}">
+            <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit sale date" name="sale_date" value="{{old('sale_date') ? : $comic->sale_date}}">
             @error('sale_date')
             <div class="invalid-feedback">
               {{$message}}
@@ -59,7 +59,7 @@
 
           <div class="form-group">
             <label for="exampleFormControlInput1">Type</label>
-            <input type="text" class="form-control @error('type') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit type" name="type" value="{{old('type')}}">
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit type" name="type" value="{{old('type') ? : $comic->type}}" required>
             @error('type')
             <div class="invalid-feedback">
               {{$message}}
@@ -69,7 +69,7 @@
 
           <div class="form-group">
             <label for="exampleFormControlInput1">Writers</label>
-            <input type="text" class="form-control @error('writers') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit writers" name="writers" value="{{old('writers')}}">
+            <input type="text" class="form-control @error('writers') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit writers" name="writers" value="{{old('writers') ? : $comic->writers}}">
             @error('writers')
             <div class="invalid-feedback">
               {{$message}}
@@ -79,7 +79,7 @@
 
           <div class="form-group">
             <label for="exampleFormControlInput1">Artists</label>
-            <input type="text" class="form-control @error('artists') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit artists" name="artists" value="{{old('artists')}}">
+            <input type="text" class="form-control @error('artists') is-invalid @enderror" id="exampleFormControlInput1" placeholder="edit artists" name="artists" value="{{old('artists') ? : $comic->artists}}">
             @error('artists')
             <div class="invalid-feedback">
               {{$message}}
@@ -92,7 +92,7 @@
         <div class="form-group">
           <label for="exampleFormControlTextarea1">Description</label>
           <textarea class="form-control @error('description') is-invalid @enderror" id="exampleFormControlTextarea1" rows="3" name="description">
-            {{old('description')}}
+            {{old('description') ? : $comic->description}}
           </textarea>
           @error('description')
           <div class="invalid-feedback">
